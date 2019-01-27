58493eddd7a50bf3393d288bfdb9e64e6a612fdd
Hồ Hữu Nhân <nhan111321@gmail.com>
2019-01-27 11:46:57 +0700

    Add files via upload

---
+++ precache-manifest.60044654eb2bb616452d8480c961f45c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c0c31bbf17d98b03ec4",
+    "url": "/static/css/main.212ba36c.chunk.css"
+  },
+  {
+    "revision": "2c0c31bbf17d98b03ec4",
+    "url": "/static/js/main.2c0c31bb.chunk.js"
+  },
+  {
+    "revision": "648f1dd97faa75992086",
+    "url": "/static/js/1.648f1dd9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ca4df803e87112bc912e151de4a2ce5",
+    "url": "/index.html"
+  }
+];
